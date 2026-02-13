d6dfc8ccb0e5777dc2d1bf2719533bfc485f0981
Peri <pglick@depaul.edu>
2026-02-13 16:56:38 -0600

    Pushing Enemy increase speed

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,16 +12,21 @@ public class EnemyScript : MonoBehaviour
     public float nextFire;
     public GameObject shootPoint;
     public string type;
+    public bool loopA;
+    public bool loopB;
+    public float shootUpTimer;
     Quaternion Up = Quaternion.LookRotation(Vector2.up);
     Quaternion Down = Quaternion.LookRotation(Vector2.down);
     Quaternion Left = Quaternion.LookRotation(Vector2.left);
     Quaternion Right = Quaternion.LookRotation(Vector2.right);
     //public float bulletSpeed;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        loopA = false;
+        loopB = false;
     }
 
     // Update is called once per frame
@@ -36,10 +42,11 @@ public class EnemyScript : MonoBehaviour
             {
                 nextFire = 0;
                 Shoot();
+                loopA = true;
             }
         }
 
-        if(gameObject.CompareTag("Fan Four"))
+       if(gameObject.CompareTag("Fan Four"))
         {
             nextFire += Time.deltaTime;
             if (nextFire > fireRate)
@@ -49,13 +56,44 @@ public class EnemyScript : MonoBehaviour
             }
         }
 
+        if (loopA)
+        {
+            StartCoroutine(waitToIncrease());
+            loopA = false;
+        }
+
+        if (loopB) 
+        { 
+           if(fireRate > 1)
+            {
+                fireRate--;
+            }
+            if (fireRate == 1)
+            {
+                fireRate = 1;
+            }
+            loopB = false;
+            
+        }
+
+
     }
+
 
 
     public void Shoot()
     {
         GameObject shot = Instantiate(projectile, shootPoint.transform.position, Quaternion.identity);
         
+    }
+
+
+    public IEnumerator waitToIncrease()
+    {
+       
+        yield return new WaitForSeconds(30);
+      
+        loopB = true;
     }
 
     public void ShootFour()
